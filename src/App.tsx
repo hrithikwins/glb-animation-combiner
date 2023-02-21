f5c0f2854c342ce61696f6557d10bee6eb288228
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-02-21 18:41:51 +0530

    model from url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Group } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter'
 import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+import styles from './App.module.css'
 
-const modelPath = '/models/scene.glb'
+// const modelPath =
 const animations = {
   Idle: '/animations/idle.glb',
   Walking: '/animations/walk.glb',
@@ -15,41 +16,118 @@ const animations = {
   RunningBackward: '/animations/runBack.glb',
   LeftStrafe: '/animations/runLeft.glb',
   RightStrafe: '/animations/runRight.glb',
-  Flying: '/animations/fly.glb'
+  Flying: '/animations/fly.glb',
+  Dancing: '/animations/sillydancing4.glb'
 }
 const gltfName = 'Armature'
 
-export default function App (): JSX.Element {
-  useEffect(() => {
-    combineAnimations().catch(console.error)
-  }, [])
+export default function App(): JSX.Element {
+  const [isFromFile, setIsFromFile] = useState<boolean>(true)
+  const [modelPath, setModelPath] = useState<string>(
+    'https://models.readyplayer.me/631f06543a656b9c323876b4.glb'
+  )
+  //   useEffect(() => {
+  //     alert('Hello')
+  //   }, [isButtonClicked])
+
+  function getTheAnimation(modelPath: string): void {
+    alert('Processing started')
+    combineAnimations(modelPath).catch(console.error)
+  }
 
   return (
-    <div>loading</div>
+    <>
+      <div className={styles.app}>
+        <div className={styles.container}>
+          <div className={styles.heading}>
+            <h1>GLTF Animation Combiner</h1>
+            <p className={styles.subHeading}>By Juuinini</p>
+          </div>
+          <div className={styles.body}>
+            {/* the selector */}
+            <div className={styles.buttonGroup}>
+              <div
+                onClick={() => setIsFromFile(true)}
+                className={
+                  styles.homeButton + ' ' + (isFromFile ? styles.isHomeActive : '')
+                }
+              >
+                From File
+              </div>
+              <div
+                onClick={() => setIsFromFile(false)}
+                className={
+                  styles.homeButton + ' ' + (!isFromFile ? styles.isHomeActive : '')
+                }
+              >
+                From Url
+              </div>
+            </div>
+            <div className={styles.formContainer}>
+              {isFromFile ? (
+                <input className={styles.inputBox} type="file" name="readyPlayerMe" />
+              ) : (
+                <input
+                  className={styles.inputBox}
+                  type="url"
+                  value="https://models.readyplayer.me/631f06543a656b9c323876b4.glb"
+                  onChange={(e) => setModelPath(e.target.value)}
+                  name="readyPlayerMe"
+                  placeholder="Enter Ready Player Me URL"
+                />
+              )}
+            </div>
+            {/* list of checkboxes */}
+            <div className={styles.checkboxContainer}>
+              {Object.keys(animations).map((animation: string, index: number) => {
+                return (
+                  <div className={styles.checkbox} key={animation + index.toString()}>
+                    <input
+                      type="checkbox"
+                      className={styles.checkboxElement}
+                      name={animation}
+                      id={animation}
+                    />
+                    <label className={styles.checkboxTitle} htmlFor={animation}>
+                      {animation}
+                    </label>
+                    <br />
+                  </div>
+                )
+              })}
+            </div>
+            {/* the button */}
+            <div className={styles.homeButton} onClick={() => getTheAnimation(modelPath)}>
+              Download
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-async function combineAnimations (): Promise<void> {
+async function combineAnimations(modelPathName: string): Promise<void> {
   const gltfLoader = new GLTFLoader()
   const gltfExporter = new GLTFExporter()
   const group = new Group()
 
   group.name = gltfName
 
-  const gltf = await asyncGltfLoad(gltfLoader, modelPath)
+  const gltf = await asyncGltfLoad(gltfLoader, modelPathName)
   group.add(gltf.scene)
 
   await loadAnimations(group, animations)
 
   gltfExporter.parse(
     gltf.scene,
-    (glb) => download(glb as ArrayBuffer, 'scene.glb'),
+    (glb) => download(glb as ArrayBuffer, modelPathName),
     console.error,
     { binary: true, animations: group.animations }
   )
 }
 
-async function loadAnimations (group: Group, animations: Object): Promise<void> {
+async function loadAnimations(group: Group, animations: Object): Promise<void> {
   const gltfLoader = new GLTFLoader()
 
   for (const [name, path] of Object.entries(animations)) {
@@ -60,7 +138,7 @@ async function loadAnimations (group: Group, animations: Object): Promise<void> 
   }
 }
 
-function download (arrayBuffer: ArrayBuffer, fileName: string): void {
+function download(arrayBuffer: ArrayBuffer, fileName: string): void {
   const blob = new Blob([arrayBuffer], { type: 'application/octet-stream' })
   const url = URL.createObjectURL(blob)
 
@@ -70,7 +148,7 @@ function download (arrayBuffer: ArrayBuffer, fileName: string): void {
   a.click()
 }
 
-async function asyncGltfLoad (gltfLoader: GLTFLoader, path: string): Promise<GLTF> {
+async function asyncGltfLoad(gltfLoader: GLTFLoader, path: string): Promise<GLTF> {
   return await new Promise((resolve, reject) => {
     gltfLoader.load(path, resolve, undefined, reject)
   })
